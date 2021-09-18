69fb610bf5906a7a388ac23386d4b90519d62b5a
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-18 20:11:09 +0530

    first commit

---
+++ src/com/bridgelabz/line/LineComparison.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.line;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Line Comparison Computation Programs ");
+        int x1 = 5;
+        int x2 = 7;
+        int y1 = 8;
+        int y2 = 89;
+
+        double lengthOfLine1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length of Line : "+lengthOfLine1);
+	}
+}
